c966cd94662a207089ff6a7ead0e1462c2fbf3a0
Pedro <pelopez@itba.edu.ar>
2021-06-11 14:50:27 -0300

    Creacion de Profile

---
+++ app/src/main/java/com/example/fitnice/Profile.java
@@ -0,0 +1,44 @@
+package com.example.fitnice;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Profile#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Profile extends Fragment {
+
+
+    public Profile() {
+        // Required empty public constructor
+    }
+    public static Profile newInstance(String param1, String param2) {
+        Profile fragment = new Profile();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ImageView profile_img = getView().findViewById(R.id.profileImg);
+        profile_img.setImageResource(R.drawable.);
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
